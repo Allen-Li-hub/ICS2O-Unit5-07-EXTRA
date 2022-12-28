0933bb82af3ae5f1f963cb78ca8ec8168cd066fb
Allen Li <113055658+Allen-Li-hub@users.noreply.github.com>
2022-12-28 18:44:04 -0500

    change

--- main.cs
+++ main.cs
@@ -1,7 +1,7 @@
 // Created by: Allen Li
-// Created on: Oct 2022
+// Created on: Dec 2022
 //
-// This program displays, "Hello, World!"
+// This program displays, "Arithmetic Progression Calculator"
 
 using System;
 
@@ -9,7 +9,27 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int userNumber;
+        int sum = 0;
+        int add = 1;
+
+        // This function accepts user input
+        Console.WriteLine("The sum of N natural numbers:");
+        Console.WriteLine(" ");
+        Console.WriteLine("Enter an positive integer below:");
+        Console.WriteLine(" ");
+
+        Console.WriteLine("Number: ");
+        userNumber = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine(" ");
+
+        for (int counter = 0; counter < userNumber; counter++)
+        {
+            sum = sum + add;
+            add = add + 1;
+        }
+        Console.WriteLine(" ");
+        Console.WriteLine("The sum is: " + sum);
         Console.WriteLine("\nDone.");
     }
 }
